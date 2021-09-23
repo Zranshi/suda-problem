6e2d5c20eb23ffdd87e348404ac135e337f3b96b
Ranshi <782938005@qq.com>
2021-09-23 14:34:48 +0800

    2008注释添加

--- src/2008/1.括号匹配/main.py
+++ src/2008/1.括号匹配/main.py
@@ -2,10 +2,26 @@
 # @Time     : 2021/7/22 10: 27
 # @Author   : Ranshi
 # @File     : main.py
-
+from typing import List, Tuple
 
 # START
-def bracket_matching(string: str):
+
+
+def bracket_matching(string: str) -> List[Tuple[int, int]]:
+    """
+    括号匹配, 给一个包含多种括号的字符串, 返回各个括号及其反括号的下标.
+
+    使用栈匹配括号. 具体算法如下:
+    - 如果当前字符是括号, 则压入栈.
+    - 如果是反括号, 则判断其和栈顶的括号是否匹配. 如果匹配则记录两者的下标, 并推出栈顶的括号
+      如果不匹配, 说明字符串是非法的括号字符串.
+
+    Args:
+        string (str): 一个包含多种括号的字符串.
+
+    Returns:
+        List[Tuple[int, int]]: 返回匹配括号的下标数组
+    """
     res, stack = [], []
     start = {"(": 1, "[": 2, "{": 3}
     end = {")": 1, "]": 2, "}": 3}
--- src/2008/3.shell排序/main.py
+++ src/2008/3.shell排序/main.py
@@ -7,7 +7,16 @@
 from typing import List
 
 
-def shell_sort(arr: List[int]):
+def shell_sort(arr: List[int]) -> List[int]:
+    """
+    希尔排序.
+
+    Args:
+        arr (List[int]): 待排序数组.
+
+    Returns:
+        List[int]: 排序完成后的数组.
+    """
     length = len(arr)
     for sub in range(length // 2, 0, -1):
         for idx in range(0, length, sub):
